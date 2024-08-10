cebe826538d21455f37120ec3b9525bb02c252fa
D-Bop <official.og.owolabi@gmail.com>
2024-08-10 23:16:30 +0100

    added more functions

--- Js/index.js
+++ Js/index.js
@@ -1,5 +1,7 @@
+alert("This page is still in progress")
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -7,16 +9,18 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
-let card = 5;
+
 
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
+    cardsEl.textContent = "Cards: " 
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
     sumEl.textContent = "Sum: " + sum;
-    
-    cardsEl.textContent = "Cards: " + firstCard + ",  "+ secondCard;
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -30,8 +34,9 @@ function renderGame() {
 }
 
 function newCard () {
-    console.log("Drawing a new card from the deck")
-    sum = sum + card;
+    let card = 5;
+    sum += card;
+    cards.push(card)
     renderGame();
 }
 
